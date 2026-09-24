db4943081f68f7b46863572f9310f843f2849088
praveens6050-afk <praveen.s6050@gmail.com>
2026-09-24 20:28:16 +0530

    Revert incomplete Seller payout webhook route

--- admin-panel/functions/api/seller-payout-webhook.js
+++
@@ -1,6 +0,0 @@
-import handler from '../../api/seller-payout-webhook.js';
-import { runNodeHandler } from '../_node-handler-bridge.js';
-
-export function onRequest(context) {
-  return runNodeHandler(handler, context);
-}
