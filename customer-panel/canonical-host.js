eea6ec2c9b24588250725add7614a8d1ba975f9b
praveens6050-afk <praveen.s6050@gmail.com>
2026-09-24 14:38:43 +0530

    Redirect customer www traffic to canonical apex host

---
+++ customer-panel/canonical-host.js
@@ -0,0 +1,6 @@
+(function(){
+'use strict';
+if(String(location.hostname||'').toLowerCase()==='www.fashionfussion.in'){
+  location.replace('https://fashionfussion.in'+location.pathname+location.search+location.hash);
+}
+})();
