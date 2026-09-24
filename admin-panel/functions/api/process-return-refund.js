a5f698e16aec3c9c4f01d4bdb698082693241ec2
praveens6050-afk <praveen.s6050@gmail.com>
2026-09-24 15:27:28 +0530

    Use Cloudflare Node bridge for return refund API

--- admin-panel/functions/api/process-return-refund.js
+++ admin-panel/functions/api/process-return-refund.js
@@ -1,6 +1,6 @@
 import handler from '../../api/process-return-refund.js';
-import { runVercelHandler } from '../_vercel-adapter.js';
+import { runNodeHandler } from '../_node-handler-bridge.js';
 
 export function onRequest(context) {
-  return runVercelHandler(handler, context);
+  return runNodeHandler(handler, context);
 }
